4eadb8aee2ef2f91418d13b7f6be00b11a59d9d4
Lisset <lissyveco@hotmail.com>
2024-12-17 18:43:21 +0100

    Add files via upload

--- Sprint4.sql
+++ Sprint4.sql
@@ -1,6 +1,7 @@
 -- Nivel 1: Dissenya una base de dades amb un esquema d'estrella:
  
-CREATE DATABASE orders; -- Creo la base de datos
+CREATE DATABASE IF NOT EXISTS orders -- Creo la base de datos
+DEFAULT CHARACTER SET = 'utf8mb4' DEFAULT COLLATE 'utf8mb4_general_ci'; 
 
 use orders; -- y la selecciono para comenzar a crear las tablas que contendrá
 
@@ -34,7 +35,7 @@ CREATE TABLE transaction( -- Tabla 1
     iban VARCHAR(40),
     pan VARCHAR(40),
     pin VARCHAR(4),
-    cvv INT,
+    cvv VARCHAR(4),
     track1 VARCHAR(50),
     track2 VARCHAR(50),
     expiring_date DATE);  
@@ -145,7 +146,7 @@ LINES TERMINATED BY '\n'   -- Fin de línea
 IGNORE 1 LINES;            -- Ignorar la primera línea (encabezados)
 
 -- Y modifico el formato del campo 'expiring_date' para volver a cambiar la variable de VARCHAR a DATE
-SET SQL_SAFE_UPDATES=0; -- para desabilitar la opción de actualización segura
+SET SQL_SAFE_UPDATES=0; -- para desabilitar la opción de actualización segura, porque sino no me deja realizar el cambio
 
 UPDATE orders.credit_cards 
 SET expiring_date = DATE_FORMAT(STR_TO_DATE(expiring_date, '%m/%d/%Y'), '%Y-%m-%d') -- str_to_date cambia las fechas de formato string a date
@@ -164,4 +165,167 @@ FIELDS TERMINATED BY ','    -- Delimitador de las columnas
 LINES TERMINATED BY '\r\n'  -- Fin de línea
 IGNORE 1 LINES;             -- Ignorar la primera línea (encabezados)
 
+-- Ahora añado las Foreign Keys que relacionaran las tablas entre ellas en este esquema de estrella
+ALTER TABLE orders.transaction
+ADD FOREIGN KEY (credit_card_id) REFERENCES credit_cards(id);
+
+ALTER TABLE orders.transaction
+ADD FOREIGN KEY (company_id) REFERENCES companies(id);
+
+ALTER TABLE orders.transaction 
+CHANGE COLUMN user_id 
+user_id INT;   -- cambio el campo 'user_id' a INT para que sea igual al campo 'id' de la tabla 'users'
+
+ALTER TABLE orders.transaction
+ADD FOREIGN KEY (user_id) REFERENCES users(id);
+
+-- Nivel 1:
+-- Ejercicio 1: Realiza una subconsulta que muestre todos los usuarios con más de 30 transacciones
+
+SELECT u.name AS nombre_usuario, u.surname AS apellido_usuario, COUNT(t.id) AS transacciones
+FROM users AS u
+JOIN transaction AS t
+ON u.id = t.user_id
+GROUP BY 1, 2
+HAVING transacciones > 30;
+
+-- Nivel 1:
+-- Ejercicio 2: Media del 'amount' por IBAN de las targetas de crédito en la compañía Donec Ltd
+
+SELECT c.company_name AS nombre_compañia, cc.id AS tarjeta_credito, cc.iban, ROUND(AVG(t.amount),2) AS importe
+FROM credit_cards AS cc
+JOIN transaction AS t
+JOIN companies AS c
+WHERE cc.id = t.credit_card_id
+AND t.company_id IN (SELECT c.id
+						FROM companies
+                        WHERE c.company_name = 'Donec Ltd')
+GROUP BY 1, 2;
+
+
+-- Nivel 2: Crea una nueva tabla que refleje el estado de las tarjetas de crédito basado en si las últimas 3 transacciones fueron declinadas.
+
+SELECT credit_card_id, SUM(declined) -- compruebo cuantas tarjetas han sido declinadas más de una vez en general
+FROM transaction
+GROUP BY 1; -- Ninguna tarjeta ha sido declinada más de una vez
+
+-- Primero creo una tabla temporal:
+CREATE TEMPORARY TABLE IF NOT EXISTS status_card AS (SELECT credit_card_id, timestamp, SUM(declined) AS total_declinadas, 
+			ROW_NUMBER() OVER (PARTITION BY credit_card_id  -- uso la función row_number() para contar el numero de transacciones por tarjeta
+								ORDER BY timestamp DESC) AS transaccion -- y lo ordeno por fecha descendiente para que las ultimas sean las primeras
+	FROM transaction
+	GROUP BY 1, 2); -- pero salen todas y solo quiero las 3 ultimas por compañia
+
+-- Y ahora si creo mi tabla con los campos que me interesan:    
+CREATE TABLE card_status AS -- Tabla 5
+SELECT credit_card_id,
+	CASE -- uso un condicional para obtener las tarjetas activas o inactivas segun las ultimas 3 transacciones declinadas
+		WHEN SUM(total_declinadas) >= 3 THEN 'inactive'
+        ELSE 'active'
+	END AS status
+FROM status_card
+WHERE transaccion <= 3 -- con este where obtengo las 3 ultimas transacciones (declinadas o no) por tarjeta
+GROUP BY 1; -- uso un condicional para obtener las tarjetas activas o inactivas segun las ultimas 3 transacciones declinadas
+
+SELECT * FROM orders.card_status; -- visualizo mi nueva tabla
+
+-- Ahora creo la PK y la FK con la que la relacionaré:
+ALTER TABLE card_status
+ADD PRIMARY KEY (credit_card_id);
+
+ALTER TABLE transaction
+ADD FOREIGN KEY (credit_card_id) REFERENCES card_status(credit_card_id);
+
+-- Nivel 2:
+-- Ejercicio 1: ¿Cuántas tarjetas están activas?
+
+SELECT COUNT(credit_card_id) AS tarjetas_activas
+FROM card_status
+WHERE status = 'active'; -- todas las tarjetas están activas
+
+SELECT COUNT(credit_card_id) AS tarjetas_inactivas
+FROM card_status
+WHERE status = 'inactive'; -- comprobamos mirando las tarjetas inactivas
+
+
+-- Nivel 3: Crea una tabla que nos permita unir los datos del archivo products.csv con la base de datos creada, teniendo en cuenta que en transaction hay product_ids. 
+
+SELECT t.id, t.products_id
+FROM transaction AS t; -- primero observamos los datos que incluiremos en la tabla
+
+-- creamos la tabla 'products':
+CREATE TABLE products( -- Tabla 6
+	id VARCHAR(10), 
+    product_name VARCHAR(40),
+    price VARCHAR(10),
+    colour VARCHAR(10),
+    weight DECIMAL(10,2),
+    warehouse_id VARCHAR(10));
+
+-- cargamos los datos de la tabla desde el archivo csv:
+LOAD DATA LOCAL INFILE '/Users/liss/Desktop/ItAcademy/2-Especialización/¡Sprints!/S4/Datos/products.csv'
+INTO TABLE orders.products
+FIELDS TERMINATED BY ','  -- Delimitador de las columnas
+LINES TERMINATED BY '\n'  -- Fin de línea
+IGNORE 1 ROWS;            -- Ignorar la primera línea (encabezados)
+
+-- visualizamos la tabla:
+SELECT * FROM orders.products;
+
+-- sustituimos los símbolos '$' de la columna price y cambiamos el tipo de dato de VARCHAR a DECIMAL:
+SET SQL_SAFE_UPDATES=0; -- para desabilitar la opción de actualización segura, porque sino no me deja realizar el cambio
+
+UPDATE products
+SET price = REPLACE(price, "$", "") 
+WHERE price IS NOT NULL;
+
+ALTER TABLE orders.products 
+CHANGE COLUMN price 
+price DECIMAL(10,2);
+
+SET SQL_SAFE_UPDATES=1; -- vuelvo a habilitar la opción de actualización segura
+
+-- elimino los espacios despues de las comas de 'products_id' de la tabla 'transaction':
+SET SQL_SAFE_UPDATES=0; -- para desabilitar la opción de actualización segura, porque sino no me deja realizar el cambio
+
+UPDATE transaction
+SET products_id = REPLACE(products_id, " ", "") 
+WHERE products_id IS NOT NULL;
+
+SET SQL_SAFE_UPDATES=1; -- vuelvo a habilitar la opción de actualización segura
+
+SELECT * FROM orders.transaction; -- visualizo los cambios
+
+-- ahora creo la tabla puente 'sales' que me relaciona las tablas 'products' y 'transaction':
+CREATE TABLE sales AS( -- tabla 7
+	SELECT t.id AS transaction_id, p.id AS product_id
+	FROM products AS p
+	JOIN transaction AS t 
+	ON FIND_IN_SET(p.id, t.products_id));
+
+-- visualizo la nueva tabla:
+SELECT * FROM orders.sales;
+
+-- y compruebo que todos los valores son correctos, sabiendo que tenemos 578 transaction_id diferentes:
+SELECT COUNT(DISTINCT transaction_id)
+FROM sales;
+
+-- creo la PK de la tabla 'products':
+ALTER TABLE products
+ADD PRIMARY KEY (id);
+
+-- le añado las FKs a la tabla 'sales' que la relacionan con 'transaction' y 'products':
+ALTER TABLE sales
+ADD FOREIGN KEY (transaction_id) REFERENCES transaction (id),
+ADD FOREIGN KEY (product_id) REFERENCES products (id);
+
+-- Nivel 3: 
+-- Ejercicio 1: Número de veces que se ha vendido cada producto
+
+SELECT p.product_name AS producto, COUNT(s.product_id) AS cantidad_de_ventas
+FROM sales AS s
+JOIN products AS p
+ON p.id = s.product_id
+GROUP BY p.id
+ORDER BY cantidad_de_ventas DESC;
 
